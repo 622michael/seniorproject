3ed4cafa621da360d77601fb2e3deafc5d2c4d3a
Michael Crabtree <mac519@lehigh.edu>
2018-11-30 16:56:07 -0500

    updated model to reflect what was trained

--- tf.py
+++ tf.py
@@ -10,23 +10,16 @@ def main():
     print(x_training.shape)
     print(y_training.shape)
     opt = tf.keras.optimizers.SGD(lr=0.1)
-    # model = tf.keras.models.Sequential([
-    #   tf.keras.layers.Conv2D(10, 25, strides=2, dilation_rate=3, activation=tf.nn.relu),
-    #   tf.keras.layres.MaxPool2D(2, 2, "same"),
-    #   tf.keras.layers.Flatten(),
-    #   tf.keras.layers.Dense(100, at)
-    #   tf.keras.Dense(1, activation=tf.keras.backend.hard_sigmoid)
-    # ])
     model = tf.keras.models.Sequential([
-      # tf.keras.layers.Conv2D(96, 5, activation=tf.nn.relu),
-      # tf.keras.layers.MaxPool2D(2, 2, "same"),
-      # tf.keras.layers.BatchNormalization(), 
-      # tf.keras.layers.Conv2D(96, 5, activation=tf.nn.relu),
-      # tf.keras.layers.MaxPool2D(2, 2, "same"),
-      # tf.keras.layers.BatchNormalization(), 
-      tf.keras.layers.Flatten(input_shape=(218, 178, 3)),
-      # tf.keras.layers.Dense(512, activation=tf.nn.relu),
-      # tf.keras.layers.Dropout(0.2),
+      tf.keras.layers.Conv2D(96, 5, activation=tf.nn.relu,input_shape=(218, 178, 3)),
+      tf.keras.layers.MaxPool2D(2, 2, "same"),
+      tf.keras.layers.BatchNormalization(), 
+      tf.keras.layers.Conv2D(96, 5, activation=tf.nn.relu),
+      tf.keras.layers.MaxPool2D(2, 2, "same"),
+      tf.keras.layers.BatchNormalization(), 
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(512, activation=tf.nn.relu),
+      tf.keras.layers.Dropout(0.2),
       tf.keras.layers.Dense(512, activation=tf.nn.sigmoid),
       tf.keras.layers.Dense(2, activation=tf.nn.softmax)
     ])
